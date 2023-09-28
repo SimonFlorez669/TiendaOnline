0ddee8eeb8d034748e494f69ec5478c49e9f6d12
Simon Florez Castellanos <140667781+SimonFlorez669@users.noreply.github.com>
2023-09-28 09:33:09 -0500

    COMMITE 28/09/2023

---
+++ TiendaOnline/src/main/java/co/edu/uco/tiendaonline/data/dao/ClienteDAO.java
@@ -0,0 +1,32 @@
+package co.edu.uco.tiendaonline.data.dao;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import co.edu.uco.tiendaonline.data.entity.ClienteEntity;
+
+public class ClienteDAO {
+	
+		
+		void crear ( ClienteEntity entity) {
+		}
+		void  modificar ( ClienteEntity entity) {
+		}
+		void eliminar( UUID id) {
+		}
+		
+		
+		Optional<ClienteEntity>consultarPorId(UUID id) {
+			return null;
+		}
+		
+		List<ClienteEntity> consultar(ClienteEntity entity) {
+			return null;
+		}
+		
+		
+
+	}
+
+
+}
---
+++ TiendaOnline/src/main/java/co/edu/uco/tiendaonline/data/dao/TipoIdentificacionDAO.java
@@ -0,0 +1,11 @@
+package co.edu.uco.tiendaonline.data.dao;
+
+public class TipoIdentificacionDAO {
+	
+	
+	
+}
+
+
+
+
