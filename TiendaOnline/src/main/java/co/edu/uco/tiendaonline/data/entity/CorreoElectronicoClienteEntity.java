ce5d29aebda399c5fb6108d9495b0a927e58a246
Simon Florez Castellanos <140667781+SimonFlorez669@users.noreply.github.com>
2023-09-27 19:17:44 -0500

    ComMIT 27/09/2023

---
+++ TiendaOnline/src/main/java/co/edu/uco/tiendaonline/data/entity/CorreoElectronicoClienteEntity.java
@@ -0,0 +1,9 @@
+package co.edu.uco.tiendaonline.data.entity;
+
+public class CorreoElectronicoClienteEntity {
+	public String correoElectronico;
+	public boolean correoElectronicoConfirmado;
+
+	public CorreoElectronicoClienteEntity() {
+	}
+}
